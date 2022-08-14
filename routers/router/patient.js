a34244a79c6ad39ab24d1e910e14859e58fbdcd0
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-08-14 17:37:24 +0300

    finish all

--- routers/router/patient.js
+++ routers/router/patient.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {getPatientById,addPatient} = require('../controllers/patient')
 const patientRouter = express.Router()
-patientRouter.post('/addPatient', addPatient)
-patientRouter.get("/getPatient/:mrn", getPatientById);
+patientRouter.post(`/addPatient`, addPatient)
+patientRouter.get(`/getPatient/:mrn`, getPatientById);
 module.exports = patientRouter
