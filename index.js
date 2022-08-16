39aa9972a3aecaa168887c235fd32700dd3b43b5
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-08-16 22:33:14 +0300

    finish all

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+dotenv.config()
 require("./db")
 app.use(express());
 app.use(cors());
@@ -10,7 +12,7 @@ app.use(morgan());
 app.use(express.json());
 const patientRouter = require('./routers/routes/patient')
 app.use(patientRouter)
-const PORT = 5000;
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
   console.log("OK");
 });
